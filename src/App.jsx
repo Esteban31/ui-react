4757de7a65a5f63ffbd8f0d13a6c6f5e3a77a3f2
Juan Esteban Calle <softwaretools731@gmail.com>
2023-04-16 08:33:23 -0500

    Login UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Detail from './pages/Detail';
+import Login from './pages/Login';
 
 const App = () => {
   return (
     <>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/" element={<Login />} />
+          <Route path="/app" element={<Home />} />
           <Route path="/detail/:id" element={<Detail />} />
         </Routes>
     </>
